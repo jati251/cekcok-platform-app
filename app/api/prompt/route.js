76d76030e203ed12970d32a949ebd418e8d85ba1
Jati Suryo Buwono <jati.buwono@ist.id>
2024-06-16 17:13:01 +0700

    fix : no cache

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,6 +1,7 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
 
+export const dynamic = 'force-dynamic';
 export const GET = async (request) => {
     try {
         await connectToDB()
