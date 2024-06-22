cb5c6ec67c5b28fc6a3b1481bc8836904b3c0f7f
Jati Suryo Buwono <jati.buwono@ist.id>
2024-06-22 08:24:52 +0700

    move url to env

--- utils/socket.js
+++ utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000", {
+const socket = io(process.env.NEXT_PUBLIC_SOCKET_SERVER_URL, {
   path: "/socket.io", // Make sure this matches the path on your server
   withCredentials: true,
   transports: ["websocket"],
